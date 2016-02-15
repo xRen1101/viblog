292b4485f802449a6bd0bbdf8dfa3462b2e29e93
xRen1101 <zairenas@gmail.com>
2016-02-15 22:23:39 +0200

    added development configurations

--- config/database.php
+++ config/database.php
@@ -55,14 +55,13 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'virelt_viblog'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', 'secret'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-            'engine'    => null,
         ],
 
         'pgsql' => [
