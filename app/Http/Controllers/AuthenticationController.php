0e412a8b7d3eaa5baab3b61e061f2000732df981
xRen1101 <zairenas@gmail.com>
2016-02-15 23:49:09 +0200

    created first rest service that gets users/user from db and returns as json

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class AuthenticationController extends Controller
+{
+    public function users($id = null)
+    {
+    	if ($id == null) {
+    		$data = User::all(array('id', 'name', 'email'));
+    	} 
+    	else {
+    		$data = User::find($id, array('id', 'name', 'email'));
+    	}
+    	
+
+    	return $data;
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('api/v1/users/{id?}', 'AuthenticationController@users');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
