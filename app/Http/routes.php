8d2a9760fe0fcc6a85fc0b378d9935030644027c
xRen1101 <zairenas@gmail.com>
2016-06-14 23:43:40 +0300

    cors

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization, auth-username, auth-password');
+
 Route::get('/', function () { return view('index'); });
 Route::get('login', function () { return view('index'); });
 
