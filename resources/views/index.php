35e9c85ce3c8243dfaec0d6b3fd57234652b2a51
xRen1101 <zairenas@gmail.com>
2016-07-21 23:11:39 +0300

    facebook post share metadata

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,46 +1,6 @@
-<!DOCTYPE html>
-<html ng-app="viblogApp">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.8">
-    <meta property="og:url" content="http://vitare.lt" />
-    <meta property="og:type" content="blog" />
-    <meta property="og:title" content="Vitare blog" />
-    <meta property="og:description" content="Whanna know more about me?" />
-    <meta property="og:image" content="http://vitare.lt/resources/assets/images/bg.jpg" />
-    <title>viBlog</title>
-    <link rel="stylesheet" type="text/css" href="resources/assets/libs/bootstrap/bootstrap.css" />
-    <link rel="stylesheet" type="text/css" href="resources/assets/libs/animate/animate.css" />
-    <link rel="stylesheet" type="text/css" href="resources/app/app.css" />
-    <base href="/">
-</head>
-<body>
-    <div ng-view></div>
-
-    <script src="resources/assets/libs/angular/angular.js"></script>
-    <script src="resources/assets/libs/angular-route/angular-route.js"></script>
-    <script src="resources/assets/libs/jquery/jquery.js"></script>
-    <script src="https://www.youtube.com/iframe_api"></script>
-    <script src="resources/assets/libs/ng-youtube-embed/ng-youtube-embed.min.js"></script>
-    <script src="resources/assets/libs/bootstrap/bootstrap.js"></script>
-    <script src="resources/assets/libs/angular-scroll/angular-scroll.js"></script>
-    <script src="resources/assets/libs/angular-inview/angular-inview.js"></script>
-    <script src="resources/assets/libs/ngScrollSpy/ngScrollSpy.js"></script>
-    <script src="resources/assets/libs/flow.js/flow.js"></script>
-    <script src="resources/assets/libs/ng-flow/ng-flow.js"></script>
-    <script src="resources\assets\libs\angular-easyfb\angular-easyfb.js"></script>
-
-    <script src="resources/app/app.js"></script>
-    <script src="resources/app/components/main/main.js"></script>
-    <script src="resources/app/components/main/mainController.js"></script>
-    <script src="resources/app/components/login/login.js"></script>
-    <script src="resources/app/components/login/loginController.js"></script>
-    <script src="resources/app/components/main/postService.js"></script>
-    <script src="resources/app/components/main/postTypeService.js"></script>
-    <script src="resources/app/components/main/notEmptyTypesFilter.js"></script>
-    <script src="resources/app/components/shared/reverseFilter.js"></script>
-    <script src="resources/app/components/shared/userService.js"></script>
-    <script src="resources/app/components/shared/pathProvider.js"></script>
-    <script src="resources/app/components/shared/imageService.js"></script>
-</body>
-</html>
+<?php
+if (preg_match("/facebook|facebot/i", $_SERVER['HTTP_USER_AGENT'])){
+   require_once('fb_crawler.php');
+} else {
+   require_once('app.php');
+}
