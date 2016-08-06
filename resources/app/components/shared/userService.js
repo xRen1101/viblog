25a6a47c748591aea5096b78ecfd900b36a6e281
xRen1101 <zairenas@gmail.com>
2016-08-06 13:57:34 +0300

    dev settings

--- resources/app/components/shared/userService.js
+++ resources/app/components/shared/userService.js
@@ -5,9 +5,9 @@ angular.module('viblogApp').factory('UserService', [
 
 	var userService = {};
 
-	userService.logged = false;
-	userService.username = '';
-	userService.password = '';
+	userService.logged = true;
+	userService.username = 'admin';
+	userService.password = 'meskiukas';
 
 	userService.login = function(username, password) {
     var deferred = $q.defer();
